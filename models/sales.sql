e646537b3370803f949c4d2499619642bd82a225
Nicolas <glaus.nicolas@gmail.com>
2023-08-02 13:20:00 +0000

    added 2 macros

--- models/sales.sql
+++ models/sales.sql
@@ -1,25 +1,27 @@
-{{ config(schema='transaction') }}
+{{ config(schema="transaction") }}
 
-WITH 
+with
 
-  sales AS (SELECT * FROM `gz_raw_data.raw_gz_sales`)
+    sales as (select * from `gz_raw_data.raw_gz_sales`),
+    product as (select * from `gz_raw_data.raw_gz_product`)
 
-  ,product AS (SELECT * FROM `gz_raw_data.raw_gz_product`)
-
-SELECT
-  s.date_date
-  ### Key ###
-  ,s.orders_id
-  ,s.pdt_id AS products_id
-  ###########
-	-- qty --
-	,s.quantity AS qty
-  -- revenue --
-  ,s.revenue AS turnover
-  -- cost --
-  ,CAST(p.purchSE_PRICE AS FLOAT64) AS purchase_price
-	,ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS purchase_cost
-	-- margin --
-	,ROUND(s.revenue-s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS product_margin
-FROM sales s
-INNER JOIN product p ON s.pdt_id = p.products_id
+select
+    s.date_date,
+    # ## Key ###
+    s.orders_id,
+    s.pdt_id as products_id,
+    # ##########
+    -- qty --
+    s.quantity as qty,
+    -- revenue --
+    s.revenue as turnover,
+    -- cost --
+    cast(p.purchse_price as float64) as purchase_price,
+    round(s.quantity * cast(p.purchse_price as float64), 2) as purchase_cost,
+    -- margin --
+    {{ margin("s.revenue", "s.quantity*CAST(p.purchSE_PRICE AS FLOAT64)") }}
+    as product_margin,
+    {{ margin_percent("s.revenue", "s.quantity*CAST(p.purchSE_PRICE AS FLOAT64)") }}
+    as product_margin_percent
+from sales s
+inner join product p on s.pdt_id = p.products_id
